e6544f8144842a98ab3524fb96744d993e991295
Rohit Banerjee <rohitban@unix6.andrew.cmu.edu>
2015-10-09 16:40:22 -0400

    COE files and python scripts to generate them

---
+++ pacman_rom/createCodeCoe.py
@@ -0,0 +1,31 @@
+radix = """MEMORY_INITIALIZATION_RADIX=16;\nMEMORY_INITIALIZATION_VECTOR="""
+
+readCount = 5
+
+
+#program to read rom files and generate a
+#byte addressable .coe file
+def main():
+
+	coe_string = radix
+	folder = "text_roms/"
+	files = ["code_rom1.txt","code_rom2.txt","code_rom3.txt","code_rom4.txt"]
+	
+	try:
+		for fileName in files:
+			path = folder+fileName
+			for line in open(path):
+				for word in line.split(" "):
+					word = word[:readCount-1]
+					coe_string += "\n" + word + ','
+	except:
+		print("File open failure")
+
+	coe_string = coe_string[:len(coe_string)-1] + ";"
+	print(coe_string)
+
+	
+
+if __name__ == "__main__":
+	main()
+    
