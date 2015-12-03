72be6afe15bf51aa046aa9e7679dd66fc4018194
Xiaofan Li <lixf@cmu.edu>
2015-12-03 11:53:48 -0500

    re-commit everything from sprite

--- video/sprite/sprite_datapath.sv
+++ video/sprite/sprite_datapath.sv
@@ -8,7 +8,7 @@ typedef struct packed{
   logic [5:0] sprite_num;
   logic      xflip;
   logic      yflip;
-} sprite_data;
+} spr_data;
 
 
 
@@ -20,7 +20,7 @@ module sprite_data_reg
      input  logic        clk, rst,
      input  logic        ld_x,ld_y,
      input  logic        ld_num_flips,ld_palette,
-     output sprite_data  d_out);
+     output spr_data  d_out);
 
     always_ff@(posedge clk, posedge rst) begin
         if(rst)
@@ -42,6 +42,42 @@ module sprite_data_reg
 endmodule: sprite_data_reg
 
 
+module sprite_reg
+    (input  logic clk, rst,
+     input  logic [7:0] din,
+     input  logic       ld_x,ld_y,ld_num_flip,ld_palette,
+     output logic [7:0] x,y,
+     output logic [7:0] palette_index,
+     output logic [5:0] sprite_num,
+     output logic       xflip,yflip);
+
+    logic [7:0] num_flip;
+
+    assign xflip = num_flip[1];
+    assign yflip = num_flip[0];
+
+    assign sprite_num = num_flip[7:2];
+    
+    always_ff@(posedge clk ,posedge rst) begin
+        if(rst) begin
+            x <= 0;
+            y <= 0;
+            num_flip <= 0;
+            palette_index <= 0;
+        end
+        else if(ld_x)
+          x <= din;
+        else if(ld_y)
+          y <= din;
+        else if(ld_num_flip)
+          num_flip <= din;
+        else if(ld_palette)
+          palette_index <= din;
+    end
+
+endmodule: sprite_reg
+
+
 //remap to screen coordinates
 module row_col_remap
     (input  logic [7:0] row,
@@ -50,9 +86,10 @@ module row_col_remap
      output logic [7:0] adj_col);
 
 
-    assign adj_row = 8'd255 - row - 8'd1;//exclude top and bottom 2 rows
+    assign adj_col = 8'd224 - row +  8'd16 ;//exclude top and bottom 2 rows
+    
 
-    assign adj_col = 8'd224 - col - 8'd1;
+    assign adj_row = 8'd255 - col + 8'd16;
 
 endmodule: row_col_remap
 
@@ -60,29 +97,97 @@ endmodule: row_col_remap
 module has_sprite
     (input  logic [7:0] sprite_x,
      input  logic [7:0] sprite_y,
-     input  logic [7:0] adj_row, adj_col,
+     input  logic [8:0] curr_row, 
+     input  logic [7:0] adj_col,
      output logic       has_sprite);
 
+    logic [7:0] adj_row;
+    
     logic row_ok, col_ok;
-
+    
+    assign adj_row = curr_row[7:0];
+    
     assign col_ok = sprite_x <= adj_col && adj_col < sprite_x + 'd16;
 
     assign row_ok = sprite_y <= adj_row && adj_row < sprite_y + 'd16;
+    
+    
     
     assign has_sprite = row_ok && col_ok;
 
 endmodule: has_sprite
 
 
+module sprite_mem_remap
+    (input  logic [15:0] RAM_addr,
+     input  logic        wr_en,
+     output logic [`NUM_SPRITES-1:0] ld_x, ld_y,
+     output logic [`NUM_SPRITES-1:0] ld_num_flip,ld_palette);
+
+    always_comb begin
+        ld_x = 0;
+        ld_y = 0;
+        ld_num_flip = 0;
+        ld_palette = 0;
+        if(wr_en) begin
+          case(RAM_addr)
+            //LOAD X
+            16'h5060: ld_x[0] = 1'b1;
+            16'h5062: ld_x[1] = 1'b1;
+            16'h5064: ld_x[2] = 1'b1;
+            16'h5066: ld_x[3] = 1'b1;
+            16'h5068: ld_x[4] = 1'b1;
+            16'h506a: ld_x[5] = 1'b1;
+            16'h506c: ld_x[6] = 1'b1;
+            16'h506e: ld_x[7] = 1'b1;
+
+            //LOAD Y
+            16'h5061: ld_y[0] = 1'b1;
+            16'h5063: ld_y[1] = 1'b1;
+            16'h5065: ld_y[2] = 1'b1;
+            16'h5067: ld_y[3] = 1'b1;
+            16'h5069: ld_y[4] = 1'b1;
+            16'h506b: ld_y[5] = 1'b1;
+            16'h506d: ld_y[6] = 1'b1;
+            16'h506f: ld_y[7] = 1'b1;
+            
+            //LOAD num and flip
+            16'h4ff0: ld_num_flip[0] = 1'b1;
+            16'h4ff2: ld_num_flip[1] = 1'b1;
+            16'h4ff4: ld_num_flip[2] = 1'b1;
+            16'h4ff6: ld_num_flip[3] = 1'b1;
+            16'h4ff8: ld_num_flip[4] = 1'b1;
+            16'h4ffa: ld_num_flip[5] = 1'b1;
+            16'h4ffc: ld_num_flip[6] = 1'b1;
+            16'h4ffe: ld_num_flip[7] = 1'b1;
+
+            //LOAD palette:
+            16'h4ff1: ld_palette[0] = 1'b1;
+            16'h4ff3: ld_palette[1] = 1'b1;
+            16'h4ff5: ld_palette[2] = 1'b1;
+            16'h4ff7: ld_palette[3] = 1'b1;
+            16'h4ff9: ld_palette[4] = 1'b1;
+            16'h4ffb: ld_palette[5] = 1'b1;
+            16'h4ffd: ld_palette[6] = 1'b1;
+            16'h4fff: ld_palette[7] = 1'b1;
+          endcase
+        end
+    end
+
+endmodule: sprite_mem_remap
+
 module sprite_datapath
-    (input  logic   clk, rst,vblank,
+    (input  logic   clk, rst,
      input  logic [8:0] row,
      input  logic [9:0] col,
 
+     //Test
+     //input logic [7:0] sw,
+
      //Read from RAM
      input  logic [7:0] sprite_RAM_din,
-     output logic       rd_en,
-     output logic [15:0]sprite_dp_RAM_addr,
+     input  logic       wr_en,
+     input logic [15:0] RAM_addr,
      
      //Output the colors that the sprite will have
      //at that position(if any)
@@ -97,11 +202,28 @@ module sprite_datapath
     
     //Register control signals
     logic [`NUM_SPRITES-1:0] ld_x, ld_y;
-    logic [`NUM_SPRITES-1:0] ld_num_flips;
+    logic [`NUM_SPRITES-1:0] ld_num_flip;
     logic [`NUM_SPRITES-1:0] ld_palette;
     
-    sprite_data [`NUM_SPRITES-1:0] sprite_info;
+    //MAP LOAD SIGNALS:       //RAM inputs
+    sprite_mem_remap spr_remap(.RAM_addr,
+                              .wr_en,
 
+                              //Load sigs
+                              .ld_x,
+                              .ld_y,
+                              .ld_num_flip,
+                              .ld_palette);
+
+    //Sprite outputs
+    logic [`NUM_SPRITES-1:0][7:0] sprite_x;
+    logic [`NUM_SPRITES-1:0][7:0] sprite_y;
+
+    logic [`NUM_SPRITES-1:0][7:0] palette_index;
+
+    logic [`NUM_SPRITES-1:0][5:0] sprite_num;
+
+    logic [`NUM_SPRITES-1:0] xflip, yflip;
 
     //remapped rows and cols(match coordinate system)
     logic [`NUM_SPRITES-1:0][7:0] adj_row, adj_col;
@@ -114,28 +236,41 @@ module sprite_datapath
     genvar i;
 
     for(i=0; i<`NUM_SPRITES;i++) begin
-      sprite_data_reg #(i) sprites(.clk,.rst,
-                                   .ld_x(ld_x[i]),
-                                   .ld_y(ld_y[i]),
-                                   .ld_num_flips(ld_num_flips[i]),
-                                   .ld_palette(ld_palette[i]),
-                            
-                                   //Data to load
-                                   .data_in(fsm_dout),
-                                   .palette_in(fsm_palette_out),
+      
+      //STORE INFO 
+      sprite_reg sreg(.clk, .rst,
+                      .din(sprite_RAM_din),
 
-                                   .d_out(sprite_info[i]));
+                      //LOAD sigs
+                      .ld_x(ld_x[i]),
+                      .ld_y(ld_y[i]),
+                      .ld_num_flip(ld_num_flip[i]),
+                      .ld_palette(ld_palette[i]),
+
+
+                      //Outputs 
+                      .x(sprite_x[i]),
+                      .y(sprite_y[i]),
+
+                      .palette_index(palette_index[i]),
+                      .sprite_num(sprite_num[i]),
+                      .xflip(xflip[i]),
+                      .yflip(yflip[i]));
 
       
-      row_col_remap rmp(.row(sprite_info[i].x_pos),
-                        .col(sprite_info[i].y_pos),
+      //REMAP coordinates
+      row_col_remap rmp(.row(sprite_x[i]),
+                        .col(sprite_y[i]),
                         .adj_row(adj_row[i]),
                         .adj_col(adj_col[i]));//transform 
                         //pacman coordinates to top left based 
                         //screen coordinates
-
+      //assign adj_row[i] = 'd32;
+      
+      //assign adj_col[i] = i*'d16;
       //check if a sprite occurs at the given screen coordinates
-      has_sprite hs(.adj_row(row[7:0]),
+      //CHECK for occurence
+      has_sprite hs(.curr_row(row[8:0]),
                     .adj_col(col[7:0]),
 
                     .sprite_x(adj_col[i]),
@@ -146,23 +281,6 @@ module sprite_datapath
     end
     endgenerate
 
-    sprite_fsm fsm(.clk,.rst,
-                   .vblank,
-
-                   //Read from RAM
-                   .sprite_RAM_din,
-                   .rd_en,
-                   .sprite_RAM_addr(sprite_dp_RAM_addr),
-
-                   //output data
-                   .fsm_dout,
-                   .palette_out(fsm_palette_out),
-
-                   //control regs
-                   .ld_x,
-                   .ld_y,
-                   .ld_num_flips,
-                   .ld_palette);
     /////////////////////////////////////////
 
 
@@ -199,46 +317,59 @@ module sprite_datapath
     //now get values for the active sprite
     logic [31:0] active_palette;
     
-    logic [5:0] sprite_num;
+    logic [5:0] active_palette_addr;
 
-    logic xflip, yflip;
+    logic [5:0] active_num;
 
-    assign sprite_num = sprite_info[active_sprite].sprite_num;
+    logic active_xflip, active_yflip;
 
-    assign xflip = sprite_info[active_sprite].xflip;
+    assign active_num = sprite_num[active_sprite];
+    
+    //assign active_num = sw[7:2];
 
-    assign yflip = sprite_info[active_sprite].yflip;
+    assign active_xflip = xflip[active_sprite];
+    
+    //assign active_xflip = sw[0];
 
-    assign active_palette = sprite_info[active_sprite].palette;
+    assign active_yflip = yflip[active_sprite];
 
+    //assign active_yflip = sw[1];
+
+    //assign active_palette_addr = palette_index[5:0];
+     
+   assign active_palette_addr = 'd3;
 
     //now get relative row and col number
     logic [7:0] row_disp, col_disp;
 
-    assign row_disp = row[7:0] - sprite_info[active_sprite].y_pos;
+    assign row_disp = row[7:0] - adj_row[active_sprite];
 
-    assign col_disp = col[7:0] - sprite_info[active_sprite].x_pos;
+    assign col_disp = col[7:0] - adj_col[active_sprite];
 
     //get the pixel number
 
     logic [7:0] pixel_num;
-    
+
     sprite_pixel_num spr_pix(.row(row_disp[3:0]),
                              .col(col_disp[3:0]),
-                             .xflip,
-                             .yflip,
+                             .xflip(~active_xflip),
+                             .yflip(~active_yflip),
                              .pixel_num);
     
     logic [11:0] pix_addr;
 
     //use this to look up sprite ROM
-    assign pix_addr = {sprite_num,pixel_num[7:2]};
+    assign pix_addr = {active_num,pixel_num[7:2]};
 
 
-    //TODO: add sprite ROM
-    
+    //SPRITE ROM
     logic [7:0] pixel_out;
-    assign pixel_out = 'd0;
+    //assign pixel_out = 0;
+    
+    sprite_rom spr_rom(.clka(clk),
+                       .addra(pix_addr),
+                       .douta(pixel_out));
+    
     ///////////////////////
 
 
@@ -249,17 +380,35 @@ module sprite_datapath
                             .pixel_data);
 
     logic [1:0] active_data;
+    
+    //Validate screen position
+    logic row_valid, col_valid, pos_valid;
+    
+    assign row_valid = (9'd16 <= row) && (row < 9'd272);
+    
+    assign col_valid  = (10'd0 <= col)&& (col<10'd224);
+    
+    assign pos_valid = row_valid&&col_valid;
+
+    assign active_data = (none_active||~pos_valid)?2'b00:pixel_data;
+        
 
 
-    assign active_data = (none_active)?2'b00:pixel_data;
+    //palette rom
+    //use active_palette_addr to access palette rom
 
+    //assign active_palette = 0;
+    
+    palette_4byte_rom spr_palette(.clka(clk),
+                                  .addra(active_palette_addr),
+                                  .douta(active_palette));
 
     //select color
     logic [7:0] palette_data;
 
     logic [7:0] color_byte;
 
-    palette_mux ps_mux(.palette(active_palette),
+    spr_palette_mux ps_mux(.palette(active_palette),
                        .pixel_data(active_data),
                        .palette_data);
 
@@ -284,7 +433,7 @@ module sprite_datapath
 
 endmodule: sprite_datapath
 
-module palette_mux
+module spr_palette_mux
     (input logic [31:0] palette,
      input logic [1:0] pixel_data,
      output logic [7:0] palette_data);
@@ -301,5 +450,5 @@ module palette_mux
      assign palette_data = palette_bytes[pixel_data];
      
      
-endmodule: palette_mux
+endmodule: spr_palette_mux
 
---
+++ video/sprite/sprite_top.sv
@@ -0,0 +1,65 @@
+`define SCRN_ROW_MAX 9'd288
+
+`define SCRN_COL_MAX 10'd224
+
+module sprite_top
+    (input  logic clk, rst,
+     
+     //input  logic [7:0] sw,
+     
+     //VGA inputs
+     //Valid rows are between 0 and 288
+     //valid cols are between 0 and 224
+     input  logic [8:0] row,
+     input  logic [9:0] col,
+     input  logic       blank,
+    
+     //RAM control 
+     input logic [7:0] sprite_RAM_din,
+     input logic       wr_en,
+     input logic [15:0] RAM_addr,
+    
+     //color outputs
+     output logic [3:0] sprite_r, sprite_g, sprite_b);     
+
+     //Determine if in range
+     //Output 0 addrs if out of range
+     logic row_ok, col_ok;
+     logic in_range;
+
+     assign row_ok = (0 <= row) && (row<`SCRN_ROW_MAX);
+     assign col_ok = (0 <= col) && (col<`SCRN_COL_MAX);
+
+     assign in_range = row_ok && col_ok;
+
+     //Get colors
+     logic [3:0] calc_r, calc_g, calc_b;
+
+     sprite_datapath spr_dat(.clk,
+                             .rst,
+
+                             //.sw,//TEST
+
+                             .wr_en,
+                             .sprite_RAM_din,
+                             .RAM_addr,
+
+
+                              //VGA connections
+                             .row,
+                             .col,
+
+
+                             //Colors
+                             .sprite_r(calc_r),
+                             .sprite_g(calc_g),
+                             .sprite_b(calc_b));
+
+      //Now assign actual color outputs
+
+      assign sprite_r = (blank||~in_range)?4'h0:calc_r;
+      assign sprite_g = (blank||~in_range)?4'h0:calc_g;
+      assign sprite_b = (blank||~in_range)?4'h0:calc_b;
+
+
+endmodule: sprite_top
